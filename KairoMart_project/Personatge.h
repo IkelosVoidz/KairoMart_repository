499e6653dc178b71eaf85302c0adbb0b4ad799dc
IkelosVoidz <114417148+IkelosVoidz@users.noreply.github.com>
2024-01-14 19:38:00 +0100

    sss

--- KairoMart_project/Personatge.h
+++ KairoMart_project/Personatge.h
@@ -2,7 +2,6 @@
 #ifndef PERSONATGE_H
 #define PERSONATGE_H
 #include <iostream>
-#include "Vehicle.h"
 using namespace std;
 
 class Personatge
@@ -12,16 +11,9 @@ public:
     Personatge(int _id, string& nom);
 
     const string getNom() const;
-    void getAdaptacio(Vehicle p); //troba el grau d'adaptacio amb el personatge
 private:
     int id;
     string nom;
-    int adaptacio;
-
 };
 
-
-
 #endif // !VEHICLE_H
-
-
