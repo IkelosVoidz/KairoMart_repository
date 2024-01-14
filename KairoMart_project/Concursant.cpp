b77499543ff690241df74bf664827dcb6a4cdc09
IkelosVoidz <114417148+IkelosVoidz@users.noreply.github.com>
2024-01-14 19:20:01 +0100

    refactoritzat concursant

--- KairoMart_project/Concursant.cpp
+++ KairoMart_project/Concursant.cpp
@@ -2,8 +2,6 @@
 Concursant::Concursant() {
 	id = 0;
 	nom = " ";
-	personatgeSelecionat = " ";
-	vehicleSeleccionat = " ";
 	posicio.first = 0;
 	posicio.second = 0;
 	voltesFetes = 0;
@@ -11,19 +9,37 @@ Concursant::Concursant() {
 Concursant::Concursant(const string& _nom, int _id) {
 	id = _id;
 	nom = _nom;
-	personatgeSelecionat = " ";
-	vehicleSeleccionat = " ";
 	posicio.first = 0;
 	posicio.second = 0;
 	voltesFetes = 0;
 }
-void Concursant::conduir() {
-	//implementar logica per moure vehicle
+
+void Concursant::Conduir()
+{
+
 }
-const string Concursant::getNom() const { return nom; }
-const int Concursant::getIdConcursant() const { return id; }
-const pair<int, int> Concursant::getPosicioConcursant() const { return posicio; }
-void Concursant::seleccionarVehicle(const string& nomVehicle) { vehicleSeleccionat = nomVehicle; }
-void Concursant::seleccionarPersonatge(const string& personatge) {
-	personatgeSelecionat = personatge;
+
+const string Concursant::getNom() const
+{
+	return nom;
+}
+
+const int Concursant::getId() const
+{
+	return id;
+}
+
+const pair<int, int> Concursant::getPosicio() const
+{
+	return posicio;
+}
+
+void Concursant::seleccionarVehicle(const Vehicle& vec)
+{
+	vehicleSeleccionat = vec;
+}
+
+void Concursant::seleccionarPersonatge(const Personatge& pers)
+{
+	personatgeSeleccionat = pers;
 }
--- KairoMart_project/Concursant.h
+++ KairoMart_project/Concursant.h
@@ -4,29 +4,31 @@
 #ifndef CONCURSANT_H
 #define CONCURSANT_H
 #include<iostream>
+#include "Vehicle.h"
+#include "Personatge.h"
+
 using namespace std;
 class Concursant
 {
 public:
 	Concursant();
 	Concursant(const string& nom, int id);
-	void conduir();
+	void Conduir();
 	const string getNom() const;
-	const int getIdConcursant() const;
-	const pair<int, int> getPosicioConcursant() const;
-	void seleccionarVehicle(const string& nomVehicle);
-	void seleccionarPersonatge(const string& personatge);
-
+	const int getId() const;
+	const pair<int, int> getPosicio() const;
+	void seleccionarVehicle(const Vehicle& vec);
+	void seleccionarPersonatge(const Personatge& pers);
 
 private:
 	string nom;
 	int id;
 	pair<int, int> posicio;
 	int voltesFetes;
-	string vehicleSeleccionat, personatgeSelecionat;
+	Vehicle vehicleSeleccionat;
+	Personatge personatgeSeleccionat;
 };
 
-
-#endif // !1
+#endif
 
 
